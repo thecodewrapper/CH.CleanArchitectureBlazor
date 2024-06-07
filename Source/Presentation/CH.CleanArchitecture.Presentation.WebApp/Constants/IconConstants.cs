7865cbef5e2448a23e3ad8506ef0fd6fabe4b454
Charalambos Hadjiantoniou <chadjiantoniou@outlook.com>
2024-06-07 13:02:57 +0300

    Removed previous icon constants

--- Source/Presentation/CH.CleanArchitecture.Presentation.WebApp/Constants/IconConstants.cs
+++ Source/Presentation/CH.CleanArchitecture.Presentation.WebApp/Constants/IconConstants.cs
@@ -6,23 +6,12 @@ namespace CH.CleanArchitecture.Presentation.WebApp.Constants
     {
         public const string ORDERS = Icons.Material.Filled.DeliveryDining;
         public const string ALL_NOTIFICATIONS = Icons.Material.Filled.Mail;
-        public const string SUBSCRIPTION_PLANS = Icons.Material.Filled.Workspaces;
-        public const string SUBSCRIPTIONS = Icons.Material.Filled.CollectionsBookmark;
-        public const string AGENTS = Icons.Material.Filled.People;
-        public const string PAYMENTS = Icons.Material.Filled.Money;
         public const string MY_NOTIFICATIONS = Icons.Material.Filled.Message;
         public const string USER_PROFILE = Icons.Material.Filled.Person;
-        public const string CUSTOMER_CARDS = Icons.Material.Filled.MedicalInformation;
-        public const string RECIPES = Icons.Material.Filled.Fastfood;
-        public const string MEAL_PLANS = Icons.Material.Filled.TableChart;
-        public const string MEAL_PLANS_REQUESTS = Icons.Material.Filled.AddAlert;
-        public const string MY_SUBSCRIPTIONS = Icons.Material.Filled.CollectionsBookmark;
-        public const string AGENT_PROFILE = Icons.Material.Filled.VerifiedUser;
         public const string DASHBOARD = Icons.Material.Filled.Home;
         public const string DEVELOPER = Icons.Material.Filled.LogoDev;
         public const string APPLICATION_CONFIG = Icons.Material.Filled.List;
         public const string AUDIT = Icons.Material.Filled.MonitorHeart;
         public const string JOBS = Icons.Material.Filled.Schedule;
-        public const string PAYMENT_GATEWAY_AUDIT = Icons.Material.Filled.Casino;
     }
 }
