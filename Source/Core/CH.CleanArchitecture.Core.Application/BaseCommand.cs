61bb2758ee10ff291616addbf38e04eaf2a6d77d
Charalambos Hadjiantoniou <chadjiantoniou@outlook.com>
2024-07-05 11:23:59 +0300

    Fix on BaseCommand

--- Source/Core/CH.CleanArchitecture.Core.Application/BaseCommand.cs
+++ Source/Core/CH.CleanArchitecture.Core.Application/BaseCommand.cs
@@ -1,18 +1,18 @@
-﻿using CH.CleanArchitecture.Common;
-using CH.Messaging.Abstractions;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
+using CH.CleanArchitecture.Common;
+using CH.Messaging.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CH.CleanArchitecture.Core.Application
 {
     public abstract class BaseCommand<TResponse> : IRequest<TResponse>, ICommand
         where TResponse : class, IResult
     {
-        public List<OperationAuthorizationRequirement> Requirements { get; private set; } = new();
+        public List<IAuthorizationRequirement> Requirements { get; private set; } = new();
         public ClaimsPrincipal User { get; set; }
 
-        protected void AddRequirements(params OperationAuthorizationRequirement[] requirements) {
+        protected void AddRequirements(params IAuthorizationRequirement[] requirements) {
             Requirements.AddRange(requirements);
         }
     }
