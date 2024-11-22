51fcb280ce7d5e8d8883509f13fce8e2f93884b6
Charalambos Hadjiantoniou <chadjiantoniou@outlook.com>
2024-11-22 10:15:30 +0200

    Added localized required attribute

---
+++ Source/Presentation/CH.CleanArchitecture.Presentation.Framework/Attributes/LocalizedRequiredAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CH.CleanArchitecture.Resources;
+
+namespace CH.CleanArchitecture.Presentation.Framework.Attributes
+{
+    public class LocalizedRequiredAttribute : RequiredAttribute
+    {
+        public LocalizedRequiredAttribute() {
+            ErrorMessageResourceType = typeof(SharedResources);
+            ErrorMessageResourceName = nameof(ResourceKeys.Validations_Required);
+        }
+    }
+}
