ccce2fd9f71dfd1a9db50b7362343dee3f49aaa4
Charalambos Hadjiantoniou <chadjiantoniou@outlook.com>
2025-05-31 23:03:46 +0300

    Enhance query handling with authorization support
    Updated `BaseQuery` to include authorization requirements and user context. Introduced `BaseQueryHandler` for processing queries with authorization checks. Implemented logging and failure response handling in the `Consume` method.

--- Source/Core/CH.CleanArchitecture.Core.Application/Base/BaseQuery.cs
+++ Source/Core/CH.CleanArchitecture.Core.Application/Base/BaseQuery.cs
@@ -1,6 +1,17 @@
-﻿namespace CH.CleanArchitecture.Core.Application
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CH.CleanArchitecture.Core.Application
 {
     public abstract class BaseQuery<TResponse> : BaseMessage<TResponse>, IQuery where TResponse : class
     {
+        public List<IAuthorizationRequirement> Requirements { get; private set; } = new();
+
+        public ClaimsPrincipal User { get; set; }
+
+        protected void AddRequirements(params IAuthorizationRequirement[] requirements) {
+            Requirements.AddRange(requirements);
+        }
     }
 }
---
+++ Source/Core/CH.CleanArchitecture.Core.Application/Base/BaseQueryHandler.cs
@@ -0,0 +1,63 @@
+﻿using CH.CleanArchitecture.Common;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CH.CleanArchitecture.Core.Application
+{
+    public abstract class BaseQueryHandler<TRequest, TResponse> : BaseMessageHandler<TRequest, TResponse>
+        where TRequest : BaseQuery<TResponse>
+        where TResponse : class, IResult
+    {
+        private readonly IAuthorizationService _authorizationService;
+        private readonly ILogger _logger;
+
+        public BaseQueryHandler(IServiceProvider serviceProvider) : base(serviceProvider) {
+            _authorizationService = serviceProvider.GetRequiredService<IAuthorizationService>();
+            _logger = serviceProvider.GetRequiredService<ILogger<TRequest>>();
+        }
+
+        public override async Task Consume(ConsumeContext<TRequest> context) {
+            IdentityContext.Initialize(context.Message.IdentityContext?.Claims);
+            var requirements = context.Message.Requirements;
+
+            // Checking authorization requirements
+            if (requirements.Any()) {
+                var user = IdentityContext.User;
+                var authorizationResult = await _authorizationService.AuthorizeAsync(user, null, requirements);
+
+                if (!authorizationResult.Succeeded) {
+                    _logger.LogError($"Authorization failed for {context.Message.GetType()}"); //push properties for failure reasons here
+                    await context.RespondAsync(CreateFailedResponse("Authorization Failed"));
+                    return;
+                }
+            }
+
+            _logger.LogDebug($"Handling {context.Message.GetType()}");
+            var messageResult = await HandleAsync(context.Message);
+            _logger.LogDebug($"Handled {context.Message.GetType()}");
+            await context.RespondAsync(messageResult);
+        }
+
+        protected TResponse CreateFailedResponse(string message, params IResultError[] errors) {
+            var result = Activator.CreateInstance<TResponse>();
+            if (result is Result r) {
+                r.IsSuccessful = false;
+                r.Message = message;
+
+                foreach (var error in errors) {
+                    r.AddError(error.Error, error.Code);
+                }
+            }
+            else {
+                throw new InvalidOperationException("TResponse must inherit from Result.");
+            }
+
+            return result;
+        }
+    }
+}
