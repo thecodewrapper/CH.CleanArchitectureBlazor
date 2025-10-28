bb1d2909f66a8aa37d5e825315261542cb02ecfb
Charalambos Hadjiantoniou <chadjiantoniou@outlook.com>
2025-10-28 16:51:36 +0200

    Add EnumerableExtensions with EmptyIfNull<T> method

---
+++ Source/CH.CleanArchitecture.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CH.CleanArchitecture.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? source) => source ?? Enumerable.Empty<T>();
+    }
+}
